cb4d09395d1065ca89229ce1ecba2d5b3de6bf9a
Hamad Safdar <hamad.safdar@emumba.com>
2024-04-29 16:09:21 +0500

    test

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-app.get("/cookie", (req, res) => {
+app.get("/", (req, res) => {
   res.cookie("express", "test", {
     sameSite: "none",
     secure: true,
     httpOnly: true,
+  });
+  res.json({
+    message: "Cookie set",
   });
 });
 
